fdcaa421810548b120a4af71ad7d572d00c1edcd
Ezeji_Chinemerem <116223274+Nemerenm@users.noreply.github.com>
2023-02-06 13:07:00 +0100

    Add files via upload

---
+++ Tuple_methods/tupletask.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=("3.4","9","banana","true")
+print(x.index("banana"))
+
+
+# In[2]:
+
+
+x=("3.4","9","banana","3.4","3.4","9","true","true")
+print(x.count("3.4"))
+
+
+# In[3]:
+
+
+print(len(x))
+
+
+# In[4]:
+
+
+x=("oge","ada","chioma","ebere","yagazie")
+print(min(x))
+
+
+# In[5]:
+
+
+print(max(x))
+
+
+# In[6]:
+
+
+print((1,2,3)+x)
+
+
+# In[7]:
+
+
+print(x*2)
+
+
+# In[8]:
+
+
+a=tuple("banana,grape,pear,oat")
+print(a)
+
+
+# In[12]:
+
+
+a=tuple(["banana","grape","pear","oat"])
+print(a)
+
+
+# In[ ]:
+
+
+
+
